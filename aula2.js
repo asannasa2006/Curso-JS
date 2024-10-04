ea3dffc3a5a251fee4f96f8f77be97ac62c74f65
asannasa2006 <biaanna104@gmail.com>
2024-10-04 09:55:30 -0300

    java if e else

---
+++ aula2.js
@@ -0,0 +1,11 @@
+let a =5;
+let b=6;
+let c=7;
+
+let soma = a+b;
+
+if (soma > c){
+    console.log("O valor " + soma +" é maior que o valor "+ c);
+}else {
+    console.log("O valor "+ soma+ " é menor que o valor "+ c);
+}
